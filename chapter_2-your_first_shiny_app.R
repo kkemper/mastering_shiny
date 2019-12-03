6ed97a208f3249c80ac8fb338b081268ea6c01dc
Kit Kemper <cgkemper@gmail.com>
2019-12-02 23:29:45 -0500

    Started exercise 4.

--- chapter_2-your_first_shiny_app.R
+++ chapter_2-your_first_shiny_app.R
@@ -74,5 +74,29 @@ shinyApp(ui, server)
 
 ### 4.
 
-#### !!Not Complete!!
+#### !!Not Complete!! - Refactor to eliminate duplicate code
 
+ui <- fluidPage(
+  sliderInput("x", "If x is", 1, 50, step = 5, value = 30),
+  sliderInput("y", "and y is", 1, 50, step = 5, value = 5),
+  "then, (x * y) is", textOutput("product"),
+  "and (x * y) + 5 is", textOutput("product_plus5"),
+  "and (x * y) + 10 is", textOutput("product_plus10")
+)
+
+server <- function(input, output, session) {
+ output$product <- renderText({
+   product <- input$x * input$y
+   product
+ })
+ output$product_plus5 <- renderText({
+   product <- input$x * input$y
+   product + 5
+ }) 
+ output$product_plus10 <- renderText({
+   product <- input$x * input$y
+   product + 10
+ })
+}
+
+shinyApp(ui, server)
