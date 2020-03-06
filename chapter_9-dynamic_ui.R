cf33e5b05ab3558063c3b27a6527a8bdc7911e78
Kit Kemper <cgkemper@gmail.com>
2020-03-06 13:16:56 -0500

    Catch up on Chapter 9
    Worked through Chapter 9 up to 9.1.5 - Exercises.
    Chapter 9 has now been moved to Chapter 8 in the actual book.
    All documents need to be updated to accomodate this change.
    
    [Ticket: X]

--- chapter_9-dynamic_ui.R
+++ chapter_9-dynamic_ui.R
@@ -60,3 +60,174 @@ shinyApp(ui, server)
 
 sales <- vroom::vroom("sales-dashboard/sales_data_sample.csv", col_types = list())
 sales
+
+ui <- fluidPage(
+  selectInput("territory", "Territory", choices = unique(sales$TERRITORY)),
+  selectInput("customername", "Customer", choices = NULL),
+  selectInput("ordernumber", "Order Number", choices = NULL),
+  tableOutput("data")
+)
+
+server <- function(input, output, session){
+  territory <- reactive({
+    req(input$territory)
+    if (input$territory == "NA") {
+      filter(sales, is.na(TERRITORY))
+    } else {
+    filter(sales, TERRITORY == input$territory)
+    }
+  })
+  observeEvent(territory(), {
+    choices <- unique(territory()$CUSTOMERNAME)
+    updateSelectInput(session, "customername", choices = choices)
+  })
+  
+  customer <- reactive({
+    req(input$customername)
+    filter(territory(), CUSTOMERNAME == input$customername)
+  })
+  observeEvent(customer(), {
+    choices <- unique(customer()$ORDERNUMBER)
+    updateSelectInput(session, "ordernumber", choices = choices)
+  })
+  
+  output$data <- renderTable({
+    req(input$ordernumber)
+    customer() %>%
+      filter(ORDERNUMBER == input$ordernumber) %>%
+      select(QUANTITYORDERED, PRICEEACH, PRODUCTCODE)
+  })
+}
+
+shinyApp(ui, server)
+
+#----------------------------------------------------
+
+### 9.1.3 - Circular References
+
+### 9.1.4 - Inter-related Inputs
+
+### 9.1.5 - Exercises
+
+#### 9.1.5.1
+library(shiny)
+ui <- fluidPage(
+  numericInput("year", "Year", value = 2020),
+  dateInput("date", "Date")
+)
+
+server <- function(input, output, session) {
+  observe({
+    yr <- input$year
+    
+    updateDateInput(session, "date", 
+                    min = paste(yr, "-01-01", sep = ""),
+                    max = paste(yr, "-12-31", sep = "")
+                    )
+  })
+  }
+
+shinyApp(ui, server)
+
+#### 9.5.1.2
+
+library(openintro)
+
+states <- unique(county$state)
+
+ui <- fluidPage(
+  selectInput("state", "State", choices = states),
+  selectInput("county", "County", choices = NULL)
+)
+
+server <- function(input, output, session){
+  st <- reactive({
+    filter(county, state == input$state)
+  })
+ observeEvent(st(), {
+   choices <- st()$name
+   updateSelectInput(session, "county", choices = choices)
+ }) 
+}
+shinyApp(ui, server)
+
+#### 9.5.1.3
+
+library(gapminder)
+continents <- unique(gapminder$continent)
+
+ui <- fluidPage(
+  selectInput("continent", "Continent", choices = continents),
+  selectInput("country", "Country", choices = NULL),
+  tableOutput("data")
+)
+
+server <- function(input, output, session){
+  cont <- reactive({
+    filter(gapminder, continent == input$continent)
+  })
+  observeEvent(cont(), {
+    choices <- cont()$country
+    updateSelectInput(session, "country", choices = choices)
+  }) 
+  
+  output$data <- renderTable({
+    req(input$country)
+    cont() %>%
+      filter(country == input$country)
+  })
+}
+shinyApp(ui, server)
+
+#### 9.5.1.4
+##### TO BE COMPLETED
+
+#### 9.5.1.4
+##### TO BE COMPLETED
+
+## 9.2 - Dynamic Visibility
+
+ui <- fluidPage(
+  tags$style("#switcher {display:none; }"),
+  sidebarLayout((
+    sidebarPanel(
+      selectInput("controller", "Show", choices = paste0("panel", 1:3))
+                  )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = "switcher",
+        tabPanel("panel1", "Panel 1 content"),
+        tabPanel("panel2", "Panel 2 content"),
+        tabPanel("panel3", "Panel 3 content")
+        )
+      )
+    )
+  )
+
+server <- function(input, output, session){
+  observeEvent(input$controller, {
+    updateTabsetPanel(session, "switcher", selected = input$controller)
+  })
+}
+
+shinyApp(ui, server)
+
+### 9.2.1  - Conditional UI
+
+parameter_tabs <- tagList(
+  tags$style("#params {display:none; }"),
+  tabsetPanel(id = "params",
+              tabPanel("normal",
+                       numericInput("mean", "mean", value = 1),
+                       numericINput("sd", "standard deviation", min = 0, value = 1)
+                       ),
+              tabPanel("uniform",
+                       numercInput("min", "min", value = 0),
+                       numericInput("max", "max", value = 1)
+                       ),
+              tabPanel("exponential",
+                       numericInput("rate", "rate", value = 1, min = 0),
+                       )
+              )
+)
