aaed03c1c1713d8bf051afa43da781c6a20de72b
Kit Kemper <cgkemper@gmail.com>
2020-03-09 20:31:59 -0400

    Through 8.2.2 - Wizard Interface

--- chapter_8-dynamic_ui.R
+++ chapter_8-dynamic_ui.R
@@ -262,3 +262,37 @@ output$hist <- renderPlot(hist(sample()))
 }
 
 shinyApp(ui, server)
+
+### 8.2.2 - Wizard Interface
+
+library(shiny)
+
+ui <- fluidPage(
+  tags$style("wizard { display:none; }"),
+  tabsetPanel(id = "wizard", 
+              tabPanel("page1", "Welcome!",
+                       actionButton("page12", "next")
+                       ),
+              tabPanel("page2", "Only one page to go",
+                       actionButton("page21", "prev"),
+                       actionButton("page23", "next")
+                       ),
+              tabPanel("page3",
+                       "You're done!",
+                       actionButton("page32", "prev")
+                       )
+              )
+)
+
+server <- function(input, output, session) {
+  switch_tab <- function(page) {
+    updateTabsetPanel(session, "wizard", selected = page)
+  }
+  
+  observeEvent(input$page12, switch_tab("page2"))
+  observeEvent(input$page21, switch_tab("page1"))
+  observeEvent(input$page23, switch_tab("page3"))
+  observeEvent(input$page32, switch_tab("page2"))
+}
+
+shinyApp(ui, server)
