423c0c69d3372723e2ecfecfa1134f3ab61ab405
Kit Kemper <cgkemper@gmail.com>
2020-03-09 21:43:26 -0400

    Work into 8.4.2 - Dynamic Filtering
    Encountering issues with the second phase of the dynamic filtering.  Not all data is appearing.  Need to refactor code, but battery is dying.
    
    [Ticket: NA]

--- chapter_8-dynamic_ui.R
+++ chapter_8-dynamic_ui.R
@@ -296,3 +296,221 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
+### 8.2.3 - Exercises
+
+#### TO BE ADDED IN BOOK
+
+## 8.3 Dialog Boxes
+
+## 8.4 - Creating UI with Code
+
+library(shiny)
+
+ui <- fluidPage(
+  textInput("label", "label"),
+  selectInput("type", "type", c("slider", "numeric")),
+  uiOutput("numeric")
+)
+
+server <- function(input, output, session) {
+  output$numeric <- renderUI({
+    if (input$type == "slider") {
+      sliderInput("dynamic", input$label, value = isolate(input$dynamic), min = 0, max = 10)
+    } else {
+      numericInput("dynamic", input$label, value = isolate(input$dynamic), min = 0, max = 10)
+    }
+  })
+}
+
+shinyApp(ui, server)
+
+### 8.4.1 - Multiple Controls
+
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+  numericInput("n", "Number of Colors", value = 5, min = 1),
+  uiOutput("col"),
+  textOutput("palette")
+)
+
+server <- function(input, output, session) {
+  col_names <- reactive(paste0("col", seq_len(input$n)))
+  
+  output$col <- renderUI({
+    map(col_names(), ~textInput(.x, NULL))
+  })
+  
+  output$palette <- renderText({
+  map_chr(col_names(), ~input[[.x]])
+  })
+}
+
+shinyApp(ui, server)
+
+
+#=============================================
+
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("n", "Number of Colors", value = 5, min = 1),
+      uiOutput("col"),
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  col_names <- reactive(paste0("col", seq_len(input$n)))
+  
+  output$col <- renderUI({
+    map(col_names(), ~textInput(.x, NULL, value = isolate(input[[.x]])) %||% "")
+  })
+  
+  output$plot <- renderPlot({
+    cols <- map_chr(col_names(), ~ input[[.x]])
+    cols[cols == ""] <- NA
+    
+    barplot(
+      rep(1, length(cols)),
+      col = cols,
+      space  = 0,
+      axes = F
+    )
+  })
+}
+
+shinyApp(ui, server)
+
+### 8.4.2 - Dynamic Filtering
+
+make_ui <- function(x, var) {
+  if (is.numeric(x)) {
+    rng <- range(x, na.rm = TRUE)
+    sliderInput(var, var, min = rng[1], max = rng[2], value = rng)
+  } else if (is.factor(x)) {
+    levs <- levels(x)
+    selectInput(var, var, choices = levs, selected = levs, multiple = TRUE)
+  } else {
+  # Not supported
+  NULL
+  }
+}
+
+filter_var <- function(x, val) {
+  if(is.numeric(x)) {
+    !is.na(x) & x >= val[1] & x <= val[2]
+  } else if (is.factor(x)) {
+    x %in% val
+  } else {
+    # No control, so don't filter
+    TRUE
+  }
+}
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      make_ui(iris$Sepal.Length, "Sepal.Length"),
+      make_ui(iris$Sepal.Width, "Sepal.Width"),
+      make_ui(iris$Species, "Species")
+    ),
+    mainPanel(
+      tableOutput("data")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  selected <- reactive({
+    filter_var(iris$Sepal.Length, input$Sepal.Length) &
+      filter_var(iris$Sepal.Width, input$Sepal.Width) &
+      filter_var(iris$Species, input$Species)
+  })
+  
+  output$data <- renderTable(head(iris[selected(), ], 12))
+}
+
+shinyApp(ui, server)
+
+
+#================================================================
+# Test Code
+
+make_ui <- function(x, var) {
+  if (is.numeric(x)) {
+    rng <- range(x, na.rm = TRUE)
+    sliderInput(var, var, min = rng[1], max = rng[2], value = rng)
+  } else if (is.factor(x)) {
+    levs <- levels(x)
+    selectInput(var, var, choices = levs, selected = levs, multiple = TRUE)
+  } else {
+    # Not supported
+    NULL
+  }
+}
+
+filter_var <- function(x, val) {
+  if (is.numeric(x)) {
+    !is.na(x) & x >= val[1] & x <= val[2]
+  } else if (is.factor(x)) {
+    x %in% val
+  } else {
+    # No control, so don't filter
+    TRUE
+  }
+}
+
+  ui <- fluidPage(
+    sidebarLayout(
+      sidebarPanel(
+        make_ui(iris$Sepal.Length, "Sepal.Length"),
+        make_ui(iris$Sepal.Width, "Sepal.Width"),
+        make_ui(iris$Species, "Species")
+      ),
+      mainPanel(
+        tableOutput("data")
+      )
+    )
+  )
+  server <- function(input, output, session) {
+    selected <- reactive({
+      filter_var(iris$Sepal.Length, input$Sepal.Length) &
+        filter_var(iris$Sepal.Width, input$Sepal.Width) &
+        filter_var(iris$Species, input$Species)
+    })
+    
+    output$data <- renderTable(head(iris[selected(), ], 12))
+  }
+  
+  shinyApp(ui, server)
+#===============================================================
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      map(names(iris), ~ make_ui(iris[[.x]], .x))
+    ),
+    mainPanel(
+      tableOutput("data")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  selected <- reactive({
+    each_var <- map(names(iris), ~ filter_var(iris[[.x]], input[[.x]]))
+    reduce(each_var, `&`)
+  })
+  
+  output$data <- renderTable(head(iris[selected(), ], 12))
+}
+
+shinyApp(ui, server)
