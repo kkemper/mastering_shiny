30ce3acd4898cc53a75896587130bbc513bcbbb6
Kit Kemper <cgkemper@gmail.com>
2020-03-10 09:58:45 -0400

    Complete work through 8.4.2 - Dynamic Filtering

--- chapter_8-dynamic_ui.R
+++ chapter_8-dynamic_ui.R
@@ -514,3 +514,38 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
+#=================================================================
+dfs <- keep(ls("package:datasets"), ~ is.data.frame(get(.x, "package:datasets")))
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dataset", label = "Dataset", choices = dfs),
+      uiOutput("filter")
+    ),
+    mainPanel(
+      tableOutput("data")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  data <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  vars <- reactive(names(data()))
+  
+  output$filter <- renderUI(
+    map(vars(), ~make_ui(data()[[.x]], .x))
+  )
+  
+  selected <- reactive({
+    each_var <- map(vars(), ~ filter_var(data()[[.x]], input[[.x]]))
+    reduce(each_var, `&`)
+  })
+  
+  output$data <- renderTable(head(data()[selected(), ], 12))
+}
+
+shinyApp(ui, server)
